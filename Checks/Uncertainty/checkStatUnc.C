b5ae16b78784ed18c8ce0dafb8a74bfd5caad68b
Andre Stahl <stahlleiton@gmail.com>
2017-11-18 04:07:35 +0100

    Updated the check Stat Toy macro

--- Checks/Uncertainty/checkStatUnc.C
+++ Checks/Uncertainty/checkStatUnc.C
@@ -9,11 +9,13 @@
 #include "TFile.h"
 #include "TCanvas.h"
 #include "TPad.h"
-#include "TGraph.h"
+#include "TGraphErrors.h"
 #include "TAxis.h"
 #include "TStyle.h"
 #include "TPaveText.h"
 #include "TLatex.h"
+#include "TLegend.h"
+#include "TLegendEntry.h"
 // RooFit headers
 #include "RooWorkspace.h"
 #include "RooDataSet.h"
@@ -29,12 +31,17 @@
 #include <string>
 
 
+typedef  std::map< std::string , std::map< std::string , std::vector< std::vector< double > > > >  ResultMap;
+
+
+void        plotResult(const ResultMap& result, const std::string& colTag, const std::string& dsTag, const std::string& outputDir);
 bool        fileList(std::vector< std::string >& fileNames, const std::string& dirPath);
 void        printElectroWeakBinning(TCanvas& pad, const RooWorkspace& ws, const std::string& dsName);
 void        setRange(RooPlot& frame);
 void        formatFrame(RooPlot& frame);
 RooRealVar  extractPar(RooPlot& frame);
 RooRealVar  extractPar(RooPlot& frame, const RooRealVar& parIni, const RooDataSet& dataIni);
+
 
 bool checkStatUnc(
                   const std::string workDirName = "NominalCM",
@@ -65,9 +72,15 @@ bool checkStatUnc(
   const std::string inputDirPath = Form("%s/Fitter/Output/%s/%s/%s/%s/result", pre2CWD.c_str(), workDirName.c_str(), metTag.c_str(), dsTag.c_str(), colTag.c_str());
   if (!fileList(inputFileNames, inputDirPath)) { return false; };
   //
+  const std::string outputDir = CWD+"/MCStatStudy_TMP/" + workDirName+"/" + metTag+"/" + dsTag+"/" + colTag+"/";
+  //
   // --------------------------------------------------------------------------------- //
   //
+  // Initialize the Output Graphs
+  ResultMap result;
+  //
   // Loop over the input files
+  //
   //
   for (const auto& inputFileName : inputFileNames) {
     //
@@ -108,18 +121,24 @@ bool checkStatUnc(
     if (ds == NULL) { std::cout << "[ERROR] The Dataset " << dsName << " was not found in the workspace" << std::endl; inputFile.Close(); return false; }
     //
     const std::string pdfName = ( "pdfMET_Tot" + tag );
-    auto pdf = (RooAddPdf*) ws->pdf(Form("%s", pdfName.c_str()));
-    if (pdf == NULL) { std::cout << "[ERROR] The PDF " << pdfName << " was not found in the workspace" << std::endl; inputFile.Close(); return false; }
+    auto pdfP = (RooAddPdf*) ws->pdf(Form("%s", pdfName.c_str()));
+    if (pdfP == NULL) { std::cout << "[ERROR] The PDF " << pdfName << " was not found in the workspace" << std::endl; inputFile.Close(); return false; }
+    auto pdf = *pdfP;
     //
-    auto var = (RooRealVar*) ws->var("MET");
-    if (var == NULL) { std::cout << "[ERROR] The variable MET was not found in the workspace" << std::endl; inputFile.Close(); return false; }
-    var->setBins(100);
+    auto varP = (RooRealVar*) ws->var("MET");
+    if (varP == NULL) { std::cout << "[ERROR] The variable MET was not found in the workspace" << std::endl; inputFile.Close(); return false; }
+    auto var = *varP;
+    var.setBins(100);
     //
-    auto par = (RooRealVar*) ws->var(Form("N_%s", tag.c_str()));
-    if (par == NULL) { std::cout << "[ERROR] The parameter " << Form("N_%s", tag.c_str()) << " was not found in the workspace" << std::endl; inputFile.Close(); return false; }
-    RooRealVar parOrig = *par;
+    auto parP = (RooRealVar*) ws->var(Form("N_%s", tag.c_str()));
+    if (parP == NULL) { std::cout << "[ERROR] The parameter " << Form("N_%s", tag.c_str()) << " was not found in the workspace" << std::endl; inputFile.Close(); return false; }
+    auto par = *parP;
     //
-    RooMCStudy mcstudy(*pdf, *var, RooFit::Binned(kFALSE), RooFit::Silence(), RooFit::Extended(), RooFit::FitOptions(RooFit::Save(), RooFit::PrintLevel(-1)));
+    bool ispPb = false; if (COL=="PA" || COL=="pPb") { ispPb = true; }
+    double etaMin = ws->var("Muon_Eta")->getMin(); if (useEtaCM) { etaMin = PA::EtaLABtoCM(etaMin, ispPb); }
+    double etaMax = ws->var("Muon_Eta")->getMax(); if (useEtaCM) { etaMax = PA::EtaLABtoCM(etaMax, ispPb); }
+    //
+    RooMCStudy mcstudy(pdf, var, RooFit::Binned(kFALSE), RooFit::Silence(), RooFit::Extended(), RooFit::FitOptions(RooFit::Save(), RooFit::PrintLevel(-1)));
     //
     // ---------------------------------------------
     // Generate and fit 1000 samples of Poisson(nExpected) events
@@ -127,7 +146,7 @@ bool checkStatUnc(
     //
     // ------------------------------------------------
     // Make plots of the distributions of Signal Yield, the error on Signal Yield and the pull of Signal Yield
-    auto framePull = mcstudy.plotPull(parOrig, RooFit::Bins(50), RooFit::Range(-5.0, 5.0), RooFit::FitGauss(kTRUE));
+    auto framePull = mcstudy.plotPull(par, RooFit::Bins(50), RooFit::Range(-5.0, 5.0), RooFit::FitGauss(kTRUE));
     //
     auto textBox = (TPaveText*)framePull->findObject("pullGauss_paramBox");
     if (textBox) {
@@ -145,13 +164,14 @@ bool checkStatUnc(
     //
     float xPos = 0.7, yPos = 0.89, dYPos = 0.045, dy = 0.025*8.;
     TLatex t = TLatex(); t.SetNDC(); t.SetTextSize(0.025);
-    t.DrawLatex(xPos, yPos-dy, Form("N_{FIT} = %.0f #pm %.0f", parOrig.getVal(), parOrig.getError())); dy+=dYPos;
-    auto framePar  = mcstudy.plotParam(parOrig);
+    t.DrawLatex(xPos, yPos-dy, Form("N_{FIT} = %.0f #pm %.0f", par.getVal(), par.getError())); dy+=dYPos;
+    auto framePar  = mcstudy.plotParam(par);
     auto toyVal = extractPar(*framePar);
     t.DrawLatex(xPos, yPos-dy, Form("#mu_{TOY} = %.0f #pm %.0f", toyVal.getVal(), toyVal.getError())); dy+=dYPos;
-    auto frameErr  = mcstudy.plotError(parOrig);
+    auto frameErr  = mcstudy.plotError(par);
     auto toyErr = extractPar(*frameErr);
     t.DrawLatex(xPos, yPos-dy, Form("#sigma_{TOY} = %.0f #pm %.0f", toyErr.getVal(), toyErr.getError())); dy+=dYPos;
+    auto toyPull = extractPar(*framePull);
     //
     printElectroWeakBinning(*cFig, *ws, dsName);
     //
@@ -160,12 +180,11 @@ bool checkStatUnc(
     CMS_lumi(cFig.get(), lumiId, 33, "");
     //
     // Save the plot in different formats
-    const std::string outputDir = CWD+"/MCStatStudy/" + workDirName+"/" + metTag+"/" + dsTag+"/" + colTag+"/";
     const std::string fileName = Form("PULL_%s_%s_%s_%s_%.0f_%.0f", "MET",
                                       tag.c_str(),
                                       dsTag.c_str(),
                                       ( ws->var("useEtaCM") ? "MuEtaCM" : "MuEta" ),
-                                      (ws->var("Muon_Eta")->getMin()*100.0), (ws->var("Muon_Eta")->getMax()*100.0)
+                                      (etaMin*100.0), (etaMax*100.0)
                                       );
     //
     gSystem->mkdir(Form("%splot/root/", outputDir.c_str()), kTRUE);
@@ -182,8 +201,18 @@ bool checkStatUnc(
     delete ws;
     inputFile.Close();
     //
+    // Add to the result container
+    if (std::abs(etaMax - etaMin)<2.0) {
+      result[CHG]["FIT"].push_back ( std::vector<double>({ ((etaMax + etaMin)/2.) , par.getVal()     , ((etaMax - etaMin)/2.) , par.getError()     }) );
+      result[CHG]["TOY"].push_back ( std::vector<double>({ ((etaMax + etaMin)/2.) , toyVal.getVal()  , ((etaMax - etaMin)/2.) , toyVal.getError()  }) );
+      result[CHG]["PULL"].push_back( std::vector<double>({ ((etaMax + etaMin)/2.) , toyPull.getVal() , ((etaMax - etaMin)/2.) , toyPull.getError() }) );
+    }
+    //
   }
   //
+  plotResult(result, colTag, dsTag, outputDir);
+  //
+  // Return
   return true;
 };
 
@@ -252,7 +281,7 @@ void printElectroWeakBinning(TCanvas& pad, const RooWorkspace& ws, const std::st
 {
   //
   char chgL = ' '; if (dsName.find("Pl")!=std::string::npos) { chgL = '+'; } else if (dsName.find("Mi")!=std::string::npos) { chgL = '-'; }
-  const std::string text = Form("#font[62]{#scale[1.1]{W^{%c}#rightarrow#mu^{%c}+x}}", chgL, chgL);
+  const std::string text = Form("#font[62]{#scale[1.1]{W^{%c}#rightarrow#mu^{%c}+#nu_{#mu}}}", chgL, chgL);
   //
   pad.cd();
   float xPos = 0.2, yPos = 0.89, dYPos = 0.045, dy = 0.025;
@@ -323,6 +352,124 @@ RooRealVar extractPar(RooPlot& frame, const RooRealVar& parIni, const RooDataSet
 };
 
 
+void updateYAxisRange(TGraphErrors& graph)
+{
+  // Find the max and min of graph
+  double yMin = 9999999999. , yMax = -9999999999.;
+  for (int i = 0; i < graph.GetN(); i++) {
+    double x, y, yL, yH;
+    graph.GetPoint(i, x, y);
+    yL = y - graph.GetErrorY(i);
+    yH = y + graph.GetErrorY(i);
+    if (yH > yMax) { yMax = yH; }
+    if (yL < yMin) { yMin = yL; }
+  }
+  // Set the up and down of y axis
+  double fMax = 0.6 , fMin = 0.1;
+  double yDown , yUp;
+  yDown = (fMax*yMin - fMin*yMax)/(fMax - fMin);
+  yUp   = yDown + (yMax-yMin)/(fMax-fMin);
+  // Update the y range
+  graph.GetYaxis()->SetRangeUser(yDown, yUp);
+};
+
+
+void plotResult(const ResultMap& result, const std::string& colTag, const std::string& dsTag, const std::string& outputDir)
+{
+  //
+  // Fill the output graphs
+  std::map< std::string , std::map< std::string , TGraphErrors > > graph;
+  //
+  for (const auto& ch : result) {
+    for (const auto& t : ch.second) {
+      const auto& b = t.second;
+      auto& gr = graph[ch.first][t.first];
+      gr.Set(b.size());
+      for (uint i = 0; i < b.size(); i++) {
+        gr.SetPoint(i, b[i][0], b[i][1]);
+        gr.SetPointError(i, b[i][2], b[i][3]);
+      }
+      //
+      gr.SetTitle("");
+      gr.GetXaxis()->SetTitle("");
+      gr.SetMarkerColor(kBlack);
+      gr.SetMarkerStyle(kFullCircle);
+      if (colTag=="PA" || colTag=="pPb") { gr.GetXaxis()->SetLimits(-3.0, 2.2); }
+      if (colTag=="Pbp") { gr.GetXaxis()->SetLimits(-2.2, 3.0); }
+      gr.GetXaxis()->SetTitleFont(42);
+      gr.GetYaxis()->SetTitleFont(42);
+      if (t.first=="PULL") {
+        gr.GetXaxis()->SetTitle("Leading Muon #eta_{CM}");
+        gr.GetXaxis()->SetTitleOffset(1);
+        gr.GetXaxis()->SetTitleSize(0.16);
+        gr.GetXaxis()->SetLabelSize(0.14);
+        gr.GetYaxis()->SetTitle("Pull");
+        gr.GetYaxis()->SetTitleOffset(0.4);
+        gr.GetYaxis()->SetTitleSize(0.16);
+        gr.GetYaxis()->SetLabelSize(0.1);
+        gr.GetYaxis()->SetRangeUser(-3.0, 3.0);
+      }
+      else {
+        gr.GetXaxis()->SetTitleSize(0.05);
+        gr.GetXaxis()->SetTitleOffset(3);
+        gr.GetXaxis()->SetLabelOffset(3);
+        gr.GetYaxis()->SetTitle(Form("N_WToMu%s_%s", ch.first.c_str(), colTag.c_str()));
+        gr.GetYaxis()->SetLabelSize(0.044);
+        gr.GetYaxis()->SetTitleSize(0.044);
+        gr.GetYaxis()->SetTitleOffset(1.7);
+        updateYAxisRange(gr);
+      }
+      if (t.first!="TOY") { gr.SetMarkerColor(kBlue); }      
+    }
+    // Draw the pads
+    TCanvas c("c", "c", 1000, 1000);
+    // Define the plotting pads
+    TPad* pad1 = new TPad("pad1", "", 0, 0.23, 1, 1);
+    TPad* pad2 = new TPad("pad2", "", 0, 0, 1, 0.228);
+    // Format the pads
+    pad2->SetTopMargin(0.02);
+    pad2->SetBottomMargin(0.4);
+    pad2->SetFillStyle(4000); 
+    pad2->SetFrameFillStyle(4000);
+    pad1->SetBottomMargin(0.015);
+    // Main Frame
+    c.cd();
+    pad1->Draw();
+    pad1->cd();
+    graph.at(ch.first).at("FIT").Draw("AP");
+    graph.at(ch.first).at("TOY").Draw("P same");
+    //
+    TLatex t = TLatex(); t.SetNDC(); t.SetTextSize(0.045);
+    char chgL = ' '; if (ch.first=="Pl") { chgL = '+'; } else if (ch.first=="Mi") { chgL = '-'; }
+    t.DrawLatex(0.23, 0.80, Form("#font[62]{#scale[1.1]{W^{%c}#rightarrow#mu^{%c}+#nu_{#mu}}}", chgL, chgL));
+    //
+    TLegend leg(0.52, 0.74, 0.52+0.2, 0.74+0.10);
+    leg.AddEntry(&graph.at(ch.first).at("FIT"), "Fitted", "p")->SetTextSize(0.040);
+    leg.AddEntry(&graph.at(ch.first).at("TOY"), "Toy", "p")->SetTextSize(0.040);
+    leg.Draw("same");
+    //
+    int lumiId = 0;
+    if (colTag=="pPb") { lumiId = 115; } else if (colTag=="Pbp") { lumiId = 116; } else if (colTag=="PA") { lumiId = 117; }
+    CMS_lumi(pad1, lumiId, 33, "");
+    gStyle->SetTitleFontSize(0.05);
+    pad1->Update();
+    // Pull Frame
+    c.cd();
+    pad2->Draw();
+    pad2->cd();
+    graph.at(ch.first).at("PULL").Draw("AP");
+    TLine line(graph.at(ch.first).at("PULL").GetXaxis()->GetXmin(), 0.0, graph.at(ch.first).at("PULL").GetXaxis()->GetXmax(), 0.0);
+    line.SetLineColor(kBlue); line.SetLineWidth(3); line.SetLineStyle(2); line.Draw("same");
+    pad2->Update();
+    // Save the plot in different formats
+    const std::string fileName = Form("RESULT_%s_%s_WToMu%s_%s", "MET", dsTag.c_str(), ch.first.c_str(), colTag.c_str());
+    //
+    c.SaveAs(Form("%splot/png/%s.png", outputDir.c_str(), fileName.c_str()));
+    c.SaveAs(Form("%splot/pdf/%s.pdf", outputDir.c_str(), fileName.c_str()));
+    c.Clear();
+    c.Close();
+  }
+};
 
 
 #endif // #ifndef checkStatUnc_C
